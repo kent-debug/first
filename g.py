1937bc58c4a9502dc590314a81ba54c9bec7bab7
kent-debug <kenneth.immunyt.health>
2024-06-14 20:52:16 +0300

    g

--- g.py
+++ g.py
@@ -1,1 +1,19 @@
-print("hello kent, welcome to github")jjffff
+import math
+
+# Given values
+population_size = 151454 
+
+z_value = 1.96
+proportion = 0.5
+margin_of_error = 0.05
+
+# Sample size calculation without FPC
+sample_size = (z_value**2 * proportion * (1 - proportion)) / (margin_of_error**2)
+
+# Applying FPC
+adjusted_sample_size = sample_size / (1 + ((sample_size - 1) / population_size))
+
+# Round up to the nearest whole number
+adjusted_sample_size = math.ceil(adjusted_sample_size)
+
+adjusted_sample_size
